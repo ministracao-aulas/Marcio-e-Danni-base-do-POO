9606f2a2f01aa531e5fa6be0d724d4ea5f8b9e7a
Tiago França <tiago@tiagofranca.com>
2023-01-11 18:22:48 -0300

    update case On branch master Changes to be committed:     modified:   custom_autoloader.php

--- custom_autoloader.php
+++ custom_autoloader.php
@@ -1,19 +1,19 @@
 <?php
 
-$to_map = require __DIR__ . '/loader_map.php';
+$toMap = require __DIR__ . '/loader_map.php';
 
-foreach ($to_map as $prefix => $base_dir) {
-    spl_autoload_register(function ($class) use ($prefix, $base_dir) {
-        $base_dir = __DIR__ . "/{$base_dir}";
+foreach ($toMap as $prefix => $baseDir) {
+    spl_autoload_register(function ($class) use ($prefix, $baseDir) {
+        $baseDir = __DIR__ . "/{$baseDir}";
 
         $len = strlen($prefix);
         if (strncmp($prefix, $class, $len) !== 0) {
             return;
         }
 
-        $relative_class = substr($class, $len);
+        $relativeClass = substr($class, $len);
 
-        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
         if (file_exists($file)) {
             require $file;
